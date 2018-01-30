3c516ca36892a552e6615484005bbcb27abc9d9b
Docroma47 <mom47@yandex.ru>
2018-01-31 00:17:00 +0300

    Add Lab 3

---
+++ Programming-laboratories/Lab3/Lab3.cxx
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x,y;
+	
+	cout<< "Please, enter X, Y value: "<< endl;
+	
+	cin>> x;
+	
+	cin>> y;
+	
+	cout<< "x + y = "<< x + y << endl;
+	
+	cout<< "x - y = "<< x - y << endl;
+	
+	cout<< "x * y = "<< x * y << endl;
+	
+	cout<< "x / y = "<< x / y << endl;
+		
+	return 0;
+	
+	
+}
+
