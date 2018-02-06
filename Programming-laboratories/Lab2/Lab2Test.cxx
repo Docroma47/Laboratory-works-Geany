3f651d507fdd510b24cfa9fc4c4e304973c17124
Docroma47 <mom47@yandex.ru>
2018-02-07 02:32:42 +0300

    Fix Test Lab2

--- Programming-laboratories/Lab2/Lab2Test.cxx
+++ Programming-laboratories/Lab2/Lab2Test.cxx
@@ -6,6 +6,19 @@ using namespace std;
 	
 int main() {
 	double result = calculate(0, 1);
+	double result1 = calculate(-1, 0);
+	double result2 = calculate(0, -1);
+	double result3 = calculate(1, 0);
+	
 	if (result != 0)
-	throw std::exception();	
+	throw std::exception();
+	
+	if (result1 != 0)
+	throw std::exception();
+		
+	if (result2 != 0)
+	throw std::exception();
+	
+	if (result3 != 0)
+	throw std::exception();
 }
--- Programming-laboratories/Lab2/MathUtils.cxx
+++ Programming-laboratories/Lab2/MathUtils.cxx
@@ -4,8 +4,6 @@
 using namespace std;
 
 double calculate(double x, double y) {
-	double sum = (sin(x) + cos(y)) / (cos(x) - sin(y)) * tan(x * y);
-	cout<< sum << endl;
 	return (sin(x) + cos(y) / cos(x) - sin(y)) * tan(x * y);
 }
 
