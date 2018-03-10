f919aff877915c61b12852fec502d064277ec0d5
Docroma47 <mom47@yandex.ru>
2018-03-10 08:34:10 +0300

    Fix test and add calculateRad and calculateDegrees

--- Programming-laboratories/Lab2/Lab2Test.cxx
+++ Programming-laboratories/Lab2/Lab2Test.cxx
@@ -3,30 +3,56 @@
 
 using namespace std;
 
-	
+
+
 int main() {
-	double result = calculate(1, 1);
-	double result1 = calculate(-1, 1);
-	double result2 = calculate(1, 2);
-	double result3 = calculate(1, 3);
+	double result = calculateDegrees(30, -5);
+	double result1 = calculateDegrees(360, 0);
+	double result2 = calculateDegrees(45, 15);
+	double result3 = calculateDegrees(130, -70);
 	
-	if ((0.01807524886 - result) > 0.000001) {
+	if ((0.90625839049 - result) > 0.0000001) {
 		cout<< "Ошибка в 1: " << result << endl;
 		throw std::exception();
 	}
 	
-	if (-(0.017460524 - result1) > 0.000001) {
+	if (result1 != 0) {
 		cout<< "Ошибка в 2: " << result1 << endl;
 		throw std::exception();
 	}
 	
-	if ((0.03677667448 - result2) > 0.000001) {
+	if ((-3.73205080757 - result2) > 0.0001) {
 		cout<< "Ошибка в 3: " << result2 << endl;
 		throw std::exception();
 	}
 	
-	if ((0.05620046266 - result3) > 0.000001) {
+	if ((21.1655118949 - result3) > 0.00001) {
 		cout<< "Ошибка в 4: " << result3 << endl;
+		throw std::exception();
+	}
+	
+	double resultRad = calculateRad(2 * pi / 3 , pi / 2);
+	double resultRad1 = calculateRad(pi / 4, pi / 6);
+	double resultRad2 = calculateRad(2 * pi, 4 * pi / 3);
+	double resultRad3 = calculateRad(5 * pi / 6, pi / 2);
+	
+	if ((-0.08623982861 - resultRad) > 0.00001) {
+		cout<< "Ошибка в calculateRad 1: " << resultRad << endl;
+		throw std::exception();
+	}
+	
+	if ((3.31249716721 - resultRad1) > 0.00001) {
+		cout<< "Ошибка в calculateRad 2: " << resultRad1 << endl;
+		throw std::exception();
+	}
+	
+	if ((-0.66201540035 - resultRad2) > 0.00001) {
+		cout<< "Ошибка в calculateRad 3: " << resultRad2 << endl;
+		throw std::exception();
+	}
+	
+	if ((-0.39161502547 - resultRad3) > 0.00001) {
+		cout<< "Ошибка в calculateRad 4: " << resultRad3 << endl;
 		throw std::exception();
 	}
 }
--- Programming-laboratories/Lab2/MathUtils.cxx
+++ Programming-laboratories/Lab2/MathUtils.cxx
@@ -5,7 +5,10 @@ using namespace std;
 
 const double pi = 3.14159265;
 
-double calculate(double x, double y) {
+double calculateDegrees(double x, double y) {
 	return ((sin(pi / 180 * x) + cos(pi / 180 * y)) / (cos(pi / 180 * x) - sin(pi / 180 * y))) * tan(pi / 180 * x * y);
 }
 
+double calculateRad(double x, double y) {
+	return ((sin(x) + cos(y)) / (cos(x) - sin(y))) * tan(x * y);
+}
